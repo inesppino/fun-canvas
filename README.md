84978c921a44a9f2df2264c161c916c083ea700b
Inés Pedraza Pino <inesppino@MacBook-Pro-de-Ines.local>
2019-08-08 09:58:55 +0200

    feat: canvas for drawing

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Fun with canvas
-
+Uso de Canvas para poder dibujar con el raton.
 # Adalab web starter kit
 Uso del Starter Kit de Adalab con node/gulp.
 Incluye SCSS, un sistema de plantillas HTMl y un web server.
--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,57 @@
 'use strict';
 
-console.log('>> Ready :)');
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 40;
+ctx.globalCompositeOperation = 'source-over';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+//let direction = true; variable necesaria para el cambio de grosor de la linea
+
+function draw(e){
+  if(!isDrawing) return; //stops the function from drawing when they are not moused down
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
+  //starts from
+  ctx.moveTo(lastX, lastY);
+  //ends
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+
+  hue++;
+  if(hue >= 360) {
+    hue = 0;
+  }
+
+// esto es para que la linea vaya cambiando de grosor
+//   if(ctx.lineWidth >= 50 || ctx.lineWidth <= 2) {
+//     direction = !direction;
+//   }
+
+//   if(direction){
+//     ctx.lineWidth++;
+//   } else {
+//     ctx.lineWidth--;
+//   }
+
+}
+
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
+
+
